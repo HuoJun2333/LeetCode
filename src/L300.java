ec9c5e5d4959f29ce848a6d09a869eb9e6bba819
xiexin <huojun2333@gmail.com>
2020-03-15 11:15:12 +0800

    增加

---
+++ src/L300.java
@@ -0,0 +1,47 @@
+/**
+ * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ *
+ * 示例:
+ *
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ * 说明:
+ *
+ * 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+ * 你算法的时间复杂度应该为 O(n2) 。
+ * 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author xiexin
+ * @create 2020-03-15 11:11 上午
+ */
+public class L300 {
+    public static void main(String[] args) {
+
+    }
+    public int lengthOfLIS(int[] nums) {
+        int maxL = 0;
+        int[] dp = new int[nums.length];
+        for(int num : nums) {
+            int lo = 0, hi = maxL;
+            while(lo < hi) {
+                int mid = lo+(hi-lo)/2;
+                if(dp[mid] < num)
+                    lo = mid+1;
+                else
+                    hi = mid;
+            }
+            dp[lo] = num;
+            if(lo == maxL) {
+                maxL++;
+            }
+        }
+        return maxL;
+    }
+}
