4ee9dd2aa295f7b29ade5bd71626870e802b5894
xiexin <huojun2333@gmail.com>
2020-03-15 11:24:42 +0800

    增加

--- src/L300.java
+++ src/L300.java
@@ -17,6 +17,8 @@
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
+import java.util.Arrays;
+
 /**
  * @author xiexin
  * @create 2020-03-15 11:11 上午
@@ -43,5 +45,30 @@ public class L300 {
             }
         }
         return maxL;
+    }
+    public int lengthOfLISCopy(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+        int[] dp = new int[len];
+        // 自己一定是一个子序列
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < len; i++) {
+            // 看以前的，比它小的，说明可以接在后面形成一个更长的子序列
+            // int curMax = Integer.MIN_VALUE; 不能这样写，万一前面没有比自己小的，
+            // 这个值就得不到更新
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int res = dp[0];
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 }
