6a944e7b0ed57dee4a0a29dd672fc69a5c4601f5
xiexin <huojun2333@gmail.com>
2020-04-07 13:15:47 +0800

    增加

---
+++ src/L0107.java
@@ -0,0 +1,96 @@
+/**
+ * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+ *
+ * 不占用额外内存空间能否做到？
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * 示例 2:
+ *
+ * 给定 matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-matrix-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author xiexin
+ * @create 2020-04-07 12:51 下午
+ */
+public class L0107 {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] matrix = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = i + j;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] +"  ");
+            }
+            System.out.println();
+        }
+
+        rotate(matrix);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] +"  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 先以对角线（左上-右下）为轴进行翻转
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        // 再对每一行以中点进行翻转
+        int mid = n >> 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < mid; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+    }
+}
